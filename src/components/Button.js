70b4f4517aa6a02768486d7818c123f70bfe6e19
yucelsedat <yucelsedatt@gmail.com>
2024-02-05 22:34:53 +0300

    props structure kullanıldı

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 
-function Button (props) {
+function Button ({text, variant}) {
 
     return (
         <button className="text-3xl font-bold bg-gray-600 text-white p-4 my-4 rounded-2xl hover:bg-gray-700 ">
-            {props.text}
+            {text}
         </button>
     );
     
