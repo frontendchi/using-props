e6208afadfd45f009790ac49245f3c80324af942
yucelsedat <yucelsedatt@gmail.com>
2024-02-05 22:32:54 +0300

    buttonlar oluşturuldu

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Button from './components/Button'
 
 function App() {
   return (
-    <div className="App">
-      <Button text="yazdır butonu"/>
+    <div className="w-full min-h-screen flex justify-center items-center gap-4">
+      <Button text="yazdır butonu" />
+      <Button text="yazdır butonu" variant="danger"/>
+      <Button text="yazdır butonu" variant="warning"/>
+      <Button text="yazdır butonu" variant="success"/>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@
 function Button (props) {
 
     return (
-        <button className="text-5xl font-bold bg-gray-800 p-8">
+        <button className="text-3xl font-bold bg-gray-600 text-white p-4 my-4 rounded-2xl hover:bg-gray-700 ">
             {props.text}
         </button>
     );
