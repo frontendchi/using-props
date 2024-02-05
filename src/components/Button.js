1e40b4f48531f1e4675a2133cfc5678d2cf3d1f8
yucelsedat <yucelsedatt@gmail.com>
2024-02-05 22:50:09 +0300

    classnames modulü ile buttonlara style verme

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,15 @@
+import classNames from "classnames";
 
-function Button ({text, variant}) {
+function Button ({text, variant = 'default'}) {
 
     return (
-        <button className="text-3xl font-bold bg-gray-600 text-white p-4 my-4 rounded-2xl hover:bg-gray-700 ">
+        <button className={classNames({
+                "text-3xl font-bold text-white p-4 my-4 rounded-2xl hover:bg-gray-700 ": true,
+                "bg-gray-600" : variant === 'default',
+                "bg-red-700" : variant === 'warning',
+                "bg-gray-800" : variant === 'danger',
+                "bg-yellow-400" : variant === 'success'
+        })}>
             {text}
         </button>
     );
